ab10a8ce402b8746b7c6e0e8a97fecacd95c37ed
Scott Nakada <scottnakada@gmail.com>
2016-10-02 11:57:07 -0700

    Pull hello template from a file.

--- app/hello.ts
+++ app/hello.ts
@@ -3,13 +3,13 @@ import {Component} from '@angular/core';
 // annotations come from TS
 @Component({
   selector: 'hello',
-//  templateUrl: './templates/hello.html',
-  template: `
-<fieldset>
-  <h2>Hello {{name}}, from inline template.</h2><br/>
-  <input [(ngModel)]="name"  type="text"/>
-  <button type="button" (click)="clickHandler()">Click Me!</button>
-</fieldset>`
+  templateUrl: './templates/hello.html',
+  //template: `
+//<fieldset>
+  //<h2>Hello {{name}}, from inline template.</h2><br/>
+  //<input [(ngModel)]="name"  type="text"/>
+  //<button type="button" (click)="clickHandler()">Click Me!</button>
+//</fieldset>`
 })
 export class HelloComponent {
   public name:string = "your name";
